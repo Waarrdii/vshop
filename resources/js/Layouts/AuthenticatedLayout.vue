13052532bfc06d26e9aa6b5f518e5083ae17634e
waarrdii <waarrdii@gmail.com>
2024-09-27 09:46:55 +0700

    chapter 4

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -33,13 +33,13 @@ const showingNavigationDropdown = ref(false);
                             <div
                                 class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
                             >
-                                <NavLink
+                                <NavLink 
                                     :href="route('dashboard')"
                                     :active="route().current('dashboard')"
                                 >
                                     Dashboard
                                 </NavLink>
-                                <NavLink
+                                <NavLink v-if="$page.props.auth.user.roles.includes('admin')"
                                     :href="route('admin.index')"
                                     :active="route().current('admin.index')"
                                 >
