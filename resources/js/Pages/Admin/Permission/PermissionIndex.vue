d2eb3c7a26d7a04e60064ee7a6de826e02afca29
waarrdii <waarrdii@gmail.com>
2024-10-05 11:34:29 +0700

    chapter 7 done

--- resources/js/Pages/Admin/Permission/PermissionIndex.vue
+++ resources/js/Pages/Admin/Permission/PermissionIndex.vue
@@ -1,14 +1,38 @@
 <script setup>
 import AdminLayout from '@/Layouts/AdminLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import TableComponent from '@/Components/TableComponent.vue';
+import TableHeader from '@/Components/TableHeader.vue';
+import TableRow from '@/Components/TableRow.vue';
+import TableData from '@/Components/TableData.vue';
+
+defineProps(['permissions'])
 </script>
 
 <template>
     <Head title="Dashboard" />
 
     <AdminLayout>
-        <div class="py-4">
-            <h1>Halaman Permission list</h1>
+        <div class="max-w-7xl mx-auto py-4">
+            <h1>Permissions List</h1>
+            <div class="mt-6">
+                <TableComponent>
+                    <template #header>
+                        <TableRow>
+                            <TableHeader>ID</TableHeader>
+                            <TableHeader>Name</TableHeader>
+                            <TableHeader>Action</TableHeader>
+                        </TableRow>
+                    </template>
+                    <template #default>
+                        <TableRow v-for="item in permissions" :key="item.id" class="border-b">
+                            <TableData>{{ item.id }}</TableData>
+                            <TableData>{{ item.name }}</TableData>
+                            <TableData>Edit/Delete</TableData>
+                        </TableRow>
+                    </template>
+                </TableComponent>
+            </div>
         </div>
     </AdminLayout>
 </template>
--- resources/js/Pages/Admin/User/UserIndex.vue
+++ resources/js/Pages/Admin/User/UserIndex.vue
@@ -1,14 +1,40 @@
 <script setup>
 import AdminLayout from '@/Layouts/AdminLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import TableComponent from '@/Components/TableComponent.vue';
+import TableHeader from '@/Components/TableHeader.vue';
+import TableRow from '@/Components/TableRow.vue';
+import TableData from '@/Components/TableData.vue';
+
+defineProps(['users'])
 </script>
 
 <template>
     <Head title="Dashboard" />
 
     <AdminLayout>
-        <div class="py-4">
-            <h1>Halaman User List</h1>
+        <div class="max-w-7xl mx-auto py-4">
+            <h1>User List</h1>
+            <div class="mt-6">
+                <TableComponent>
+                    <template #header>
+                        <TableRow>
+                            <TableHeader>ID</TableHeader>
+                            <TableHeader>Name</TableHeader>
+                            <TableHeader>Email</TableHeader>
+                            <TableHeader>Action</TableHeader>
+                        </TableRow>
+                    </template>
+                    <template #default>
+                        <TableRow v-for="user in users" :key="user.id" class="border-b">
+                            <TableData>{{ user.id }}</TableData>
+                            <TableData>{{ user.name }}</TableData>
+                            <TableData>{{ user.email }}</TableData>
+                            <TableData>Edit/Delete</TableData>
+                        </TableRow>
+                    </template>
+                </TableComponent>
+            </div>
         </div>
     </AdminLayout>
 </template>
