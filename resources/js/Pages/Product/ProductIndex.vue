41bb17e6c3e7df951c92d61bfbd2a16a22074b27
waarrdii <waarrdii@gmail.com>
2024-10-06 22:49:28 +0700

    progress chapter 8, n try make customs CRUD

---
+++ resources/js/Pages/Product/ProductIndex.vue
@@ -0,0 +1,36 @@
+<script setup>
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import TableComponent from '@/Components/TableComponent.vue';
+import TableHeader from '@/Components/TableHeader.vue';
+import TableRow from '@/Components/TableRow.vue';
+import TableData from '@/Components/TableData.vue';
+
+defineProps(['products'])
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AdminLayout>
+        <div class="max-w-7xl mx-auto py-4">
+            <h1>Product List</h1>
+            <div class="mt-6">
+                <TableComponent>
+                    <template #header>
+                        <TableRow>
+                            <TableHeader>ID</TableHeader>
+                            <TableHeader>Name</TableHeader>
+                        </TableRow>
+                    </template>
+                    <template #default>
+                        <TableRow v-for="item in products" :key="item.id" class="border-b">
+                            <TableData>{{ item.id }}</TableData>
+                            <TableData>{{ item.name }}</TableData>
+                        </TableRow>
+                    </template>
+                </TableComponent>
+            </div>
+        </div>
+    </AdminLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -35,6 +36,7 @@ Route::middleware(['auth','role:admin'])->group(function () {
 Route::resource('/users', UserController::class);
 Route::resource('/roles', RoleController::class);
 Route::resource('/permissions', PermissionController::class);
+Route::resource('/products', ProductController::class);
 
 
 require __DIR__.'/auth.php';
